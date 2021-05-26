f673b8ba1b5a65bcefa0fde4f1dfdcb5655bb3ca
Austin Wong <austinwong@umail.ucsb.edu>
2021-05-25 19:13:31 -0700

    Add Basic Shuffle Component

---
+++ src/Components/ShufflePage.js
@@ -0,0 +1,29 @@
+import {Component} from 'react'
+import Button from 'react-bootstrap/Button';
+
+class ShufflePage extends Component{
+
+    constructor() {
+        super();
+        this.state = {
+          lcd: ['DDM', 'Dorie', 'Jacob', 'Graham', 'Evan', 'Ivailo', 'Kim', 'Austin']
+        }
+        this.setState({lcd: this.state.lcd.sort(() => Math.random() - 0.5)})
+      }
+      
+    shuffleMembers(){
+        this.setState({lcd: this.state.lcd.sort(() => Math.random() - 0.5)})
+    }
+    
+    render(){
+        return(
+            <div class="d-flex flex-column align-items-center">
+                <button class="btn btn-primary m-1" onClick={this.shuffleMembers.bind(this)}>Shuffle</button>
+                    {this.state.lcd.map((member) => 
+                        <h1>{member}</h1>)}
+            </div>
+        )
+    }
+}
+
+export default ShufflePage
